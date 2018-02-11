da9a9acf89906909d21bac62aa899b2a84eeaa6d
mdarfler <mdarfler@gmail.com>
2018-02-11 15:38:57 -0500

    Transpile to ES6

--- chp02_forces/NOC_2_08_mutualattraction/mover.js
+++ chp02_forces/NOC_2_08_mutualattraction/mover.js
@@ -2,38 +2,40 @@
 // Daniel Shiffman
 // http://natureofcode.com
 
-var Mover = function(m, x, y) {
-  this.mass = m;
-  this.position = createVector(x, y);
-  this.velocity = createVector(0, 0);
-  this.acceleration = createVector(0, 0);
-   
-  this.applyForce = function(force) {
+class Mover{
+  constructor(m,x,y){
+    this.mass = m;
+    this.position = createVector(x, y);
+    this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
+  }
+
+  applyForce(force) {
     var f = p5.Vector.div(force, this.mass);
     this.acceleration.add(f);
   };
-    
-  this.update = function() {
+
+  update() {
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
     this.acceleration.mult(0);
   };
 
-  this.display = function() {
+  display() {
     stroke(0);
     strokeWeight(2);
     fill(255, 127);
     ellipse(this.position.x, this.position.y, this.mass*16, this.mass*16);
   };
 
-  this.calculateAttraction = function(m) {
+  calculateAttraction(m) {
     // Calculate direction of force
     var force = p5.Vector.sub(this.position, m.position);
     // Distance between objects
     var distance = force.mag();
     // Limiting the distance to eliminate "extreme" results for very close or very far objects
     distance = constrain(distance, 5.0, 25.0);
-    // Normalize vector (distance doesn't matter here, we just want this vector for direction                            
+    // Normalize vector (distance doesn't matter here, we just want this vector for direction
     force.normalize();
     // Calculate gravitional force magnitude
     var strength = (G * this.mass * m.mass) / (distance * distance);
@@ -42,7 +44,3 @@ var Mover = function(m, x, y) {
     return force;
   };
 };
-
-
-
-
