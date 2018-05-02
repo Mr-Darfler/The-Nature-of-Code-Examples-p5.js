ec116681f2859b3807deda7e4c8bc1d5e4bdb147
mdarfler <mdarfler@gmail.com>
2018-05-02 09:08:58 -0400

    Start GA for Class

---
+++ chp09_ga/NOC_9_01_GA_Shakespeare_From_Scratch/DNA.js
@@ -0,0 +1,29 @@
+function newChar() {
+  
+}
+
+// Constructor (makes a random DNA)
+class DNA {
+  // The genetic sequence
+  constructor(num){
+
+  }
+
+  // Converts character array to a String
+  getPhrase() {
+    return this.genes.join("");
+  }
+
+  // Fitness function (returns floating point % of "correct" characters)
+  calcFitness(target) {
+  }
+
+  // Crossover
+  crossover(partner) {
+
+  }
+
+  // Based on a mutation probability, picks a new random character
+  mutate(mutationRate) {
+  }
+}
---
+++ chp09_ga/NOC_9_01_GA_Shakespeare_From_Scratch/Population.js
@@ -0,0 +1,54 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Genetic Algorithm, Evolving Shakespeare
+
+// A class to describe a population of virtual organisms
+// In this case, each organism is just an instance of a DNA object
+
+class Population {
+  constructor(p, m, num) {
+
+  }
+
+
+
+  // Fill our fitness array with a value for every member of the population
+  calcFitness() {}
+
+  // Generate a mating pool
+  naturalSelection() {
+
+  }
+
+  // Create a new generation
+  generate() {
+
+  }
+
+
+  getBest() {
+
+  }
+
+  // Compute the current "most fit" member of the population
+  evaluate() {}
+
+  isFinished() {
+
+  }
+
+  getGenerations() {
+
+  }
+
+  // Compute average fitness for the population
+  getAverageFitness() {
+
+  }
+
+  allPhrases() {
+
+  }
+}
