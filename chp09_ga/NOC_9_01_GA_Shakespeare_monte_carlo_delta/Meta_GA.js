b96697da1922ab1e9e5531716a415a23484df3d5
mdarfler <mdarfler@gmail.com>
2018-05-06 15:28:05 -0400

    First Attempt. Build MetaGA and implement in Sketch

---
+++ chp09_ga/NOC_9_01_GA_Shakespeare_monte_carlo_delta/Meta_GA.js
@@ -0,0 +1,45 @@
+class GARun {
+  constructor(targetPhrase, mutationRate, popSize) {
+    this.numGens = [];
+    this.population;
+    this.targetPhrase = targetPhrase;
+    this.mutationRate = mutationRate;
+    this.popSize = popSize;
+    this.rateOfChange = 1;
+    this.finished = false;
+  }
+
+  createGA() {
+    this.population = new Population(this.targetPhrase, this.mutationRate, this.popSize)
+  }
+  evolve() {
+    while(!this.population.isFinished()){
+      this.population.calcFitness();
+      this.population.evaluate();
+      this.naturalSelection();
+      this.generate();
+      if(this.population.generations > 1000) break;
+    }
+    this.numGens.push(this.population.generations);
+
+
+  }
+  rateOfChange() {
+    const runLength = 10;
+    if (this.numGens.length > runLength) {
+      this.numGens.shift();
+      let runningAve = this.numGens.reduce((a, cv) => a + cv);
+      runningAve = runningAve / runLength;
+
+      const last = this.numGens[this.numGens.length - 1];
+      this.rateOfChange = abs((last - runningAve) / runningAve);
+    }
+  }
+
+  isFinished(threshold){
+    if(this.rateOfChange < threshold) this.finished = true;
+    return this.finished;
+    }
+  }
+
+}
--- chp09_ga/NOC_9_01_GA_Shakespeare_monte_carlo_delta/sketch.js
+++ chp09_ga/NOC_9_01_GA_Shakespeare_monte_carlo_delta/sketch.js
@@ -1,30 +1,31 @@
 let target;
-let popmax;
+let popSize;
 let mutationRate;
-let population;
-let numGenerations = [];
-let aveGen = [0];
-let iterator = 0;
-let monteCarlo = 100;
-let w;
-let runningAve;
-const maxGens = 1000;
+let metaGA;
+// let aveGen = [0];
+// let iterator = 0;
+// let monteCarlo = 100;
+// let w;
+// let runningAve;
+// const maxGens = 1000;
 
 
 function setup() {
 
-  createCanvas(600, 400);
-  background(255);
-  strokeWeight(4);
-  stroke(0);
-  rect(0, 0, width, height);
-  runningAve = createP('Running Average: ')
-
-  w = width/monteCarlo;
-
+  //createCanvas(600, 400);
+  //background(255);
+  // strokeWeight(4);
+  // stroke(0);
+  // rect(0, 0, width, height);
+  // runningAve = createP('Running Average: ')
+  //
+  // w = width/monteCarlo;
+  //
   target = "To be or not to be.";
   popMax = 800;
   mutationRate = 0.01;
+
+  metaGA = new MetaGA(target,mutationRate,popMax);
 
 
 
@@ -37,69 +38,9 @@ function setup() {
 }
 
 function draw() {
-
-  let tStart = millis();
-
-    population = new Population(target, mutationRate, popMax);
-    population.calcFitness();
-
-    while (!population.isFinished()) {
-      population.naturalSelection();
-      //Create next generation
-      population.generate();
-      // Calculate fitness
-      population.calcFitness();
-
-      population.evaluate();
-
-      if (population.generations > 1000) break;
-    }
-
-    strokeWeight(1);
-    stroke(100);
-    fill(150);
-    let h = map(population.generations, 0, maxGens, 0, height);
-    rectMode(CORNER)
-
-    rect(iterator * w, height - h, w, h);
-    numGenerations.push(population.generations);
-    //}
-    let s,ave;
-    s = numGenerations.reduce((s, c) => s + c);
-    ave = s/(iterator+1)
-    aveGen.push(ave);
-
-    strokeWeight(5);
-    stroke(255,0,0);
-
-    let x1 = iterator * w;
-    let y1 = map(aveGen[iterator],0,maxGens,height,0);
-    let x2 = x1 + w;
-    let y2 = map(ave,0,maxGens,height,0);
-    line(x1,y1,x2,y2);
-
-    runningAve.html("Running Average: "+round(ave));
-    console.log((millis()-tStart)/1000);
-
-    // println("pop size: " + popMax);
-    // println("Mutation Rate: " + mutationRate);
-    // println("Ave # of Generations: " + sum / numGenerations.length);
-
-    iterator++
-    if(iterator > monteCarlo) {
-      noLoop();
-      println("Average Generations after " + monteCarlo + " generations: " + ave)
-      console.log('done')
-    }
-
+  metaGa.createGA();
+  metaGa.evolve();
+  console.log(metaGA.getAveGens());
+  noLoop()
 
 }
-
-// function changing(pop){
-//   let changing = false();
-//   if(pop.getGenerations()>10){
-//     if(){
-//
-//     }
-//   }
-// }
